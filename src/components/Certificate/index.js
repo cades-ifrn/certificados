51db841e99f35003929abba214768caf5ed957c5
Felipe Pontes <felipemfpontes@gmail.com>
2018-09-20 21:41:14 -0300

    Add template for committee

--- src/components/Certificate/index.js
+++ src/components/Certificate/index.js
@@ -3,25 +3,25 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 import TemplateSimple from "./TemplateSimple";
+import TemplateCommittee from "./TemplateCommittee";
 
-const TEMPLATE_SIMPLE = 1;
-// more templates
+const TEMPLATE = {
+  1: TemplateSimple,
+  2: TemplateCommittee
+  // more templates
+};
 
 export default ({ id, eventData, participantData }) => {
-  let Template = null;
+  if (!!TEMPLATE[eventData.template]) {
+    const Template = TEMPLATE[eventData.template];
 
-  switch (eventData.template) {
-    case TEMPLATE_SIMPLE:
-      Template = TemplateSimple;
-      break;
-    default:
-      return <Redirect to={"/404"} />;
+    return (
+      <Template
+        id={id}
+        {...TemplateSimple.selector(eventData, participantData)}
+      />
+    );
   }
 
-  return (
-    <Template
-      id={id}
-      {...TemplateSimple.selector(eventData, participantData)}
-    />
-  );
+  return <Redirect to={"/404"} />;
 };
--- src/data/events.js
+++ src/data/events.js
@@ -1,12 +1,14 @@
 import cadesday1Git from "./cadesday1-git.json";
 import cadesday1Laravel from "./cadesday1-laravel.json";
 import cadesday1Ux from "./cadesday1-ux.json";
+import cadesdayOrg from "./cadesday1-organizacao.json";
 
 // be careful
 const events = {
   1: cadesday1Git,
   2: cadesday1Laravel,
-  3: cadesday1Ux
+  3: cadesday1Ux,
+  4: cadesdayOrg
 };
 
 export { events };
